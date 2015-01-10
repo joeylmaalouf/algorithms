9bdf953e3169bd4ac4da1d8f37177e23beee0d43
Joey L. Maalouf <joeylmaalouf@gmail.com>
2015-01-10 18:14:19 -0500

    began string module

---
+++ string.py
@@ -0,0 +1,26 @@
+# IMPORT -----------------------------------------------------------------------
+import sys
+# ------------------------------------------------------------------------------
+
+
+# CLEAN ------------------------------------------------------------------------
+def clean(string):
+    """ Returns a string with all punctuation replaced with spaces.
+    """
+    translation = string.maketrans(
+        "!#$%&()*+,-.:;<=>?@[]^_`{|}~'\\/\"",
+        "                                ")
+    return string.translate(translation)
+# ------------------------------------------------------------------------------
+
+
+# UNIT TEST --------------------------------------------------------------------
+def main(argv):
+    string = "The fox & the cat jumped over the big, lazy dog! Oh no."
+    print(string)
+    print(clean(string))
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+# ------------------------------------------------------------------------------
