caa84a59e71e9a3feb0e15a9f3c68515a9d076be
Joey L. Maalouf <joeylmaalouf@gmail.com>
2015-01-10 18:27:57 -0500

    updated string.clean

--- string.py
+++ string.py
@@ -5,18 +5,20 @@ import sys
 
 # CLEAN ------------------------------------------------------------------------
 def clean(string):
-    """ Replaces all punctuation in a string with spaces.
+    """ Replaces all punctuation in a string with spaces,
+    then trims down any extra spaces in a row.
     """
     translation = string.maketrans(
         "!#$%&()*+,-.:;<=>?@[]^_`{|}~'\\/\"",
         "                                ")
-    return string.translate(translation)
+    string = string.translate(translation)
+    return " ".join(string.split())
 # ------------------------------------------------------------------------------
 
 
 # UNIT TEST --------------------------------------------------------------------
 def main(argv):
-    string = "The fox & the cat jumped over the big, lazy dog! Oh no."
+    string = "The fox & the c@ jumped over the big + lazy dog! Oh, no."
     print(string)
     print(clean(string))
 
